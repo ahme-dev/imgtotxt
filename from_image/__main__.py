979dfa09b18e9fbe9288ced76dde619cfe01052b
Ahmed K.A <ahmadkabdullah@pm.me>
2023-03-26 02:27:05 +0300

    Move app start and exit to app.py

--- from_image/__main__.py
+++ from_image/__main__.py
@@ -1,27 +1,4 @@
 """"main module"""
+from from_image.app import run_app
 
-import sys
-import toga
-from from_image.ui import create_ui
-
-
-def run_app():
-    """run the app"""
-    return toga.App(
-        formal_name="From Image",
-        app_id="systems.ahmed.from-image",
-        app_name="from-image",
-        author="https://ahmed.systems",
-        startup=create_ui,
-        on_exit=exit_app,
-    )
-
-
-def exit_app(_):
-    """exit the app"""
-    print("Exited")
-    sys.exit(0)
-
-
-# run the main loop
 run_app().main_loop()
