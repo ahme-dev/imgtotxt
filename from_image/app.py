ad8b8c9afbb852e3325041752e22e29b2f4e2c28
Ahmed K.A <ahmadkabdullah@pm.me>
2023-03-26 14:24:48 +0300

    Clear out ui and merge question dialog with exit handler

--- from_image/app.py
+++ from_image/app.py
@@ -6,72 +6,59 @@ from from_image.ocr import LANGUAGES, load_reader, read_from_image
 
 class FromImageApp(toga.App):
     def startup(self):
+        # create main window
         self.main_window = toga.MainWindow(title=self.name)
-        self.on_exit = self.action_question_dialog
 
+        # create image
         image = toga.ImageView(toga.Image("../mock/note.png"))
         image.style.update(height=300, width=300)
 
-        box = toga.Box(
-            children=[
-                toga.Box(
-                    style=Pack(direction=ROW, alignment=CENTER, padding=10),
-                    children=[
-                        toga.Box(
-                            style=Pack(direction=COLUMN, alignment=CENTER),
-                            children=[
-                                image,
-                                toga.Button(
-                                    "Open file",
-                                    on_press=self.action_open_file_filtered_dialog,
-                                ),
-                            ],
-                        ),
-                        toga.Box(children=[toga.Label("hello")]),
-                    ],
-                )
-            ]
-        )
-        # self.on_exit = self.exit_handler
+        box = toga.Box(children=[])
+
+        # hook on exit handler, add content to main window and show
+        self.on_exit = self.action_question_dialog
         self.main_window.content = box
         self.main_window.show()
 
     async def action_open_file_filtered_dialog(self, _):
+        """open a file dialog with filetype filters"""
         try:
+            # open a dialog and get filepath
             fname = await self.main_window.open_file_dialog(
-                title="Open file with Toga",
+                title="Open an image to read text from",
                 multiselect=False,
                 file_types=["png", "jpg", "jpeg"],
             )
-            if fname is not None:
-                self.main_window.title = f"File to open: {fname}"
-            else:
+
+            # if no file was selected return
+            if fname is None:
                 self.main_window.title = "No file selected!"
+                return
+
+            self.main_window.title = f"File to open: {fname}"
         except ValueError:
             self.main_window.text = "Open file dialog was canceled"
 
     async def action_question_dialog(self, _):
+        """on app exit ask for confirmation"""
         if await self.main_window.question_dialog(
             "FromImage", "Do you want to exit the app?"
         ):
-            self.exit_handler()
+            print("Exited")
+            sys.exit(0)
         else:
             return
 
-    def click_handler(self, btn):
-        reader, error = load_reader(["en", "ar", "ku"])
+    # def click_handler(self, btn):
+    #     reader, error = load_reader(["en", "ar", "ku"])
 
-        if isinstance(error, Exception):
-            print(error)
-            btn.text = "Error"
-            return
+    #     if isinstance(error, Exception):
+    #         print(error)
+    #         btn.text = "Error"
+    #         return
 
-        read_from_image(reader, "mock/note.png")
-        btn.text = "Read"
-
-    def exit_handler(self):
-        print("Exited")
-        sys.exit(0)
+    #     read_from_image(reader, "mock/note.png")
+    #     btn.text = "Read"
 
 
 def run_app():
